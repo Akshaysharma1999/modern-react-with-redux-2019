2f041f1e927b67c4535e04347b9ed4bf17a22ae8
Steve Tipton <slaatipton26@gmail.com>
2019-01-29 13:17:06 -0500

    Update seasons to use hooks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,42 +3,30 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 import Spinner from './Spinner'
+import useLocation from './useLocation'
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' }
+const App = () => {
+  const [lat, errorMessage] = useLocation()
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
-    )
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return (
-        <div className="error ui negative message">
-          <i className="icon-left massive meh icon" />
-          <div className="header">
-            <h1>Ser Ilynn, bring me his head!!</h1>
-            <p>{this.state.errorMessage}</p>
-          </div>
-          <i className="icon-right massive cut icon" />
+  let content
+  if (errorMessage) {
+    content = (
+      <div className="error ui negative message">
+        <i className="icon-left massive meh icon" />
+        <div className="header">
+          <h1>Ser Ilynn, bring me his head!!</h1>
+          <p>{errorMessage}</p>
         </div>
-      )
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
-    }
-
-    return <Spinner message="Please accept location request" />
+        <i className="icon-right massive cut icon" />
+      </div>
+    )
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner message="Please accept location request" />
   }
 
-  // React says we have to define render!!
-  render() {
-    return <div className="border red">{this.renderContent()}</div>
-  }
+  return <div className="border red">{content}</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
